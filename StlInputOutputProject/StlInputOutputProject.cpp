9ced2c5146e7b0dac7fe476f67524f5e6f5e9025
Ефимов Максим <efimov_m@itstep.academy>
2022-12-11 17:56:20 +0300

    Добавьте файлы проекта.

---
+++ StlInputOutputProject/StlInputOutputProject.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <fstream>
+
+
+int main()
+{
+    std::fstream file;
+
+    /*file.open("file.txt", std::ios::out);
+    std::string str = "Hello world";
+    file.write(str.c_str(), str.length());
+    file.seekp(-6, std::ios::end);
+    file.put('!');
+
+    file << "Hello people";
+    file.close();*/
+
+    file.open("file.txt", std::ios::in);
+    
+    /*std::string sout(10, ' ');
+    file.read(&sout[0], 10);
+    std::cout << sout << "\n";
+    std::cout << file.get() << "\n";
+
+    std::string sout2;
+    file >> sout2;
+    std::cout << sout2 << "\n";*/
+
+    while (!file.eof())
+    {
+        std::string buffer(1024, ' ');
+        file.getline(&buffer[0], 1024);
+        buffer.resize(file.gcount());
+        std::cout << buffer << "\n";
+        /*file >> buffer;
+        std::cout << buffer;
+        if (buffer == "\n")
+            std::cout << "\n";*/
+    }
+
+    file.close();
+    return 0;
+}
